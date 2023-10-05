4c3f733a8c932994fc49ac343fd36dc0c064d3bc
HakunV <hakun_vestergaard@hotmail.com>
2023-10-05 15:46:03 +0200

    Problem 2 Solved

--- Field.java
+++ Field.java
@@ -6,18 +6,25 @@
 
 public class Field {
 
-    public Field() {
+    Semaphore[][] listOfS = new Semaphore[11][12];
 
+    public Field() {
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 12; j++) {
+                listOfS[i][j] = new Semaphore(1);
+            }
+
+        }
     }
 
     /* Block until car no. may safely enter tile at pos */
     public void enter(int no, Pos pos) throws InterruptedException {
-
+        listOfS[pos.row][pos.col].P();
     }
 
     /* Release tile at position pos */
     public void leave(Pos pos) {
-
+        listOfS[pos.row][pos.col].V();
     }
 
 }
