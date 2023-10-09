24b63544bc6bd5b8d2d6389b1fcd875180345e1b
Rasmus Østfeldt <rasmus@oestfeldt.net>
2023-10-09 16:52:13 +0200

    battonalley

--- BatonAlley.java
+++ BatonAlley.java
@@ -5,27 +5,28 @@
 //Hans Henrik Lovengreen     Sep 26, 2023
 
 public class BatonAlley extends Alley {
+    Semaphore[] baton = new Semaphore[1];
 
     protected BatonAlley() {
-        
+        baton[0] = new Semaphore(1);
     }
 
     /* Block until car no. may enter alley */
     public void enter(int no) throws InterruptedException {
         if (no < 5) {
-            
+            baton[0].P();
         } else {
-            
+            baton[0].P();
         }
 
-     }
+    }
 
     /* Register that car no. has left the alley */
     public void leave(int no) {
         if (no < 5) {
-
+            baton[0].V();
         } else {
-
+            baton[0].V();
         }
     }
 
